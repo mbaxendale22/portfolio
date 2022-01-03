e136e3cb7b755097a136166516b15622532d2f82
Matthew Baxendale <mdbaxendale1@gmail.com>
2022-01-03 20:16:00 +0000

    links added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-blue-500">Testing</h1>
+      <h1 className="text-blue-500">Matthew Baxendale</h1>
+      <h3>Junior Full Stack Developer</h3>
+      <div>
+        <Link href="/">
+          <a>About</a>
+        </Link>
+        <Link href="/">
+          <a>Projects</a>
+        </Link>
+        <Link href="/">
+          <a>Contact</a>
+        </Link>
+      </div>
     </div>
   );
 }
