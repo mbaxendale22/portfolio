d42d9fdaa948c1a15b4a88681b08515ca8cada1d
Matthew Baxendale <mdbaxendale1@gmail.com>
2022-01-05 16:39:17 +0000

    re-pushing files

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,9 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center font-Montserrat gap-24'>
-    <div className='flex flex-col border gap-4'>
+    <div className='flex flex-col border gap-4 justify-center items-center'>
       <h1 className="text-8xl">Matthew Baxendale</h1>
-      <h3 className='text-center text-xl'>Junior Full Stack Developer</h3>
+      <h3 className='text-xl'>Junior Full Stack Developer</h3>
     </div>
       <div className='flex justify-evenly w-1/2 border text-xl'>
         <Link href="/about">
